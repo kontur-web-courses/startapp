bf8b5a7573d69d71fb23d48ea51f51183be1196a
Домашних Иван <digi@skbkontur.ru>
2021-02-27 22:04:06 +0500

    Reformat with Prettier

--- WebApp/ClientApp/src/setupTests.ts
+++ WebApp/ClientApp/src/setupTests.ts
@@ -2,7 +2,7 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 const localStorageMock = {
   getItem: jest.fn(),
@@ -10,7 +10,7 @@ const localStorageMock = {
   removeItem: jest.fn(),
   clear: jest.fn(),
   length: 0,
-  key: jest.fn()
+  key: jest.fn(),
 };
 global.localStorage = localStorageMock;
 
@@ -18,14 +18,16 @@ global.localStorage = localStorageMock;
 window.fetch = (input: RequestInfo, init?: RequestInit) => {
   const response = {
     ok: true,
-    json: () => Promise.resolve({
-      "authority": "https://localhost:5001",
-      "client_id": "WebApp",
-      "redirect_uri": "https://localhost:5001/authentication/login-callback",
-      "post_logout_redirect_uri": "https://localhost:5001/authentication/logout-callback",
-      "response_type": "id_token token",
-      "scope": "WebAppAPI openid profile"
-   })
+    json: () =>
+      Promise.resolve({
+        authority: "https://localhost:5001",
+        client_id: "WebApp",
+        redirect_uri: "https://localhost:5001/authentication/login-callback",
+        post_logout_redirect_uri:
+          "https://localhost:5001/authentication/logout-callback",
+        response_type: "id_token token",
+        scope: "WebAppAPI openid profile",
+      }),
   } as Response;
   return Promise.resolve(response);
 };
