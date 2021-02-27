992a0711db8cec4064659c4a24490d67b57fdc58
Домашних Иван <digi@skbkontur.ru>
2021-02-27 21:43:19 +0500

    Database regeneration for Development

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -13,7 +14,9 @@ namespace WebApp
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.PrepareDB();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
